137e7b2a92a4119632b64887f0693eb0822f5081
Solohill <solomonhill0620@gmail.com>
2022-06-02 15:38:44 -0400

    Calculator

---
+++ src/main/if_else.py
@@ -0,0 +1,17 @@
+val1 = input("What is the first value?")
+val2 = input("What is the second value?")
+operation = input("What do you want to do next?")
+if "+" == operation:
+    print(int(val1) + int(val2))
+if "-" == operation:
+    print(int(val1) - int(val2))
+
+if "%" == operation:
+    print(int(val1) % int(val2))
+
+
+
+
+
+
+
---
+++ src/main/main_application.py
@@ -0,0 +1,19 @@
+user_input_prompt = "What is your age?"
+user_input_as_string = input(user_input_prompt)
+
+
+user_input_as_int = int(user_input_as_string)
+
+
+print("user_input_as_string: " + user_input_as_string)
+print("user_input_as_int: " + str(user_input_as_int))
+
+age = input("What is your age?")
+name = input("What's your name?")
+print("Nice to meet you " + name)
+print(name + " is " + age + " years old")
+
+x = 5
+y = 2
+print (x+y)
+
